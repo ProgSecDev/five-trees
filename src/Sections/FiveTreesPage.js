385f8bbe9832283d9041d3ef70d3067b809518eb
Elie Ephram <elie_93AF@hotmail.com>
2026-05-05 10:19:49 +0300

    Final Update

--- src/Sections/FiveTreesPage.js
+++ src/Sections/FiveTreesPage.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { Element } from "react-scroll";
 import Home from "./HomePage/Home";
-import About from "./About Us/AboutUs";
 import Footer from "../Components/Footer";
 
 const FiveTreesPage = () => {
